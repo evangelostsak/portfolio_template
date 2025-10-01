f2409d79bfc5ec4d66e9f9ade137191bd1ba3179
evangelostsak <baggtsak55@gmail.com>
2025-10-02 01:06:53 +0200

    Add image remote patterns for Credly and Readme Typing SVG

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.credly.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'readme-typing-svg.herokuapp.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
